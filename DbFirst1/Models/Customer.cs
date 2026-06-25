d367bff2e802d3b8dc37792a0b3d0db6568a3ee7
sanket-phadatare <sanketphadatare1718@gmail.com>
2026-06-25 11:46:43 +0530

    Opearation with Db First approach

---
+++ DbFirst1/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirst1.Models;
+
+public partial class Customer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+
+    public int Age { get; set; }
+}
